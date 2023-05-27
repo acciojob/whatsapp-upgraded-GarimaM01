f713f341d5989586d79815b550049cdac7c3bcdd
GarimaM01 <garima.mahto@sakec.ac.in>
2023-05-27 13:35:48 +0530

    1st commit

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -2,6 +2,29 @@ package com.driver;
 
 public class Group {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
     private int numberOfParticipants;
+    public Group() {
+    }
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
 
 }
